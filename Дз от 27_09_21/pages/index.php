c563e70ea172cd2265f3b888188f9e3f7c87a804
Eldar Abdullin <shpast@yandex.ru>
2021-10-14 21:32:21 +0500

    дз от 13_10_21

--- Дз от 27_09_21/pages/index.php
+++ Дз от 27_09_21/pages/index.php
@@ -1,26 +1,48 @@
 <?
 //1.
 $strAboutMe = 'О себе:<br> заканчиваю обучение в Яндекс Практикуме,поступил в школу ФАКТ. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam id maiores quis voluptatum? Adipisci alias aperiam, consequuntur deserunt dicta, eligendi esse eum harum, hic inventore iste laborum libero nam pariatur.';
-$arrAboutMe = explode('.',$strAboutMe);
-$firstElementStr = array_shift($arrAboutMe);
-$firstElementStr = '<span style="color:green">' . $firstElementStr . '</span>';
-$arrAboutMe = implode($arrAboutMe);
+function strFirstChange($str){
+    $arr = explode('.', $str);
+    $firstElementStr = array_shift($arr);
+    $firstElementStr = '<span style="color:green">' . $firstElementStr . '</span>';
+    $arr = implode($arr);
+    return $firstElementStr . $arr;
+}
 $strFeedback = ' Все нравится!&#128077; Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aut deleniti dolores, eaque eos harum itaque iure iusto laudantium neque obcaecati perspiciatis placeat quia rem reprehenderit saepe sed sint temporibus!';
-$arrFeedback = explode(' ', $strFeedback);
+function strSecondChange($str)
+{
+    $arr = explode(' ', $str);
+    foreach ($arr as $key => $value) {
+        if ($key % 2 === 0) {
+            echo '<span style="color:blue">' . $value . ' ' . '</span>';
+        } else {
+            echo '<span style="color:green">' . $value . ' ' . '</span>';
+        }
+    }
+}
 //2.
 $strAll = 'Личная страничка студента. Массивы. Регистрация. Эльдар Абдуллин. ' . $strAboutMe . $strFeedback . 'Любимые фильмы Дэвида Финчера. Бойцовский клуб. 7емь. Социальная сеть. Загадочная история Бенджамина Баттона. Любимые фильмы Гая Ричи. Карты, деньги, два ствола. Большой куш. Шерлок Холмс. Джентльмены. Количество гласных букв на сайте: Количество слов на сайте: День рожденья: Сегодня: Разница в днях составляет: ';
-$strAllNew = mb_strtolower($strAll);
-$arrAll = explode(' ', $strAll);
-//$arrVowelLetters = ['а','о','э','е','и','ы','у','ё','ю','я','a','e','i','o','u','y'];
-//$countVowelLetters = 0;
-//$countWord = 0;
-//echo $countVowelLetters . ' ' . $countWord;
-$strVowelCount = substr_count($strAllNew, 'а') + substr_count($strAllNew, 'о') + substr_count($strAllNew, 'э') + substr_count($strAllNew, 'е') + substr_count($strAllNew, 'и') + substr_count($strAllNew, 'ы') + substr_count($strAllNew, 'у') + substr_count($strAllNew, 'ё') + substr_count($strAllNew, 'ю') + substr_count($strAllNew, 'я') + substr_count($strAllNew, 'f') + substr_count($strAllNew, 'e') + substr_count($strAllNew, 'i') + substr_count($strAllNew, 'o') + substr_count($strAllNew, 'u') + substr_count($strAllNew, 'y');//не смог сделать, чтобы у меня работала функция mb_str_split, пришлось использовать такой костыль =(
-$date = '30.11.1992';
-$date2 = DateTime::createFromFormat('d.m.Y',$date); // Преобразовываем дату в необходимый формат
-$date1 = new DateTime(); // Сейчас
-$dayDiff = $date2->diff($date1)->format('%a');
+function vowelCount($str){
+    $newStr = mb_strtolower($str);
+    return $strVowelCount = substr_count($newStr, 'а') + substr_count($newStr, 'о') + substr_count($newStr, 'э') + substr_count($newStr, 'е') + substr_count($newStr, 'и') + substr_count($newStr, 'ы') + substr_count($newStr, 'у') + substr_count($newStr, 'ё') + substr_count($newStr, 'ю') + substr_count($newStr, 'я') + substr_count($newStr, 'f') + substr_count($newStr, 'e') + substr_count($newStr, 'i') + substr_count($newStr, 'o') + substr_count($newStr, 'u') + substr_count($newStr, 'y');
+}
+function wordCount($str){
+    return str_word_count($str);
+}
+$Birthday = '30.11.1992';
+function dayDiff($date){
+    $date2 = DateTime::createFromFormat('d.m.Y',$date); // Преобразовываем дату в необходимый формат
+    $date1 = new DateTime(); // Сейчас
+    return $dayDiff = $date2->diff($date1)->format('%a');
+}
 $dateToday = date('d.m.Y');
+function fibonachi($x, $y){
+    if($x <= 10000){
+        echo $x . ' ';
+        $z = $x + $y . ' ';
+        fibonachi($y, $z);
+    }
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -40,14 +62,8 @@ require_once('header.php');
     <section class="about-me">
     <img class="img border" src="../images/eldar-abdullin.jpeg" alt="Фото" title="фото">
     <h2 class="name border">Эльдар Абдуллин</h2>
-    <p class="about border"><?echo $firstElementStr . $arrAboutMe?></p>
-    <p class="feedback border"><? foreach ($arrFeedback as $key => $value){
-            if($key % 2 === 0){
-                echo '<span style="color:blue">' . $value . ' ' . '</span>';
-            }else{
-                echo '<span style="color:green">' . $value . ' ' . '</span>';
-            }
-        }?></p>
+    <p class="about border"><?echo strFirstChange($strAboutMe)?></p>
+    <p class="feedback border"><? strSecondChange($strFeedback)?></p>
     </section>
     <section class="fincher">
         <h2 class="film_title">Любимые фильмы Дэвида Финчера</h2>
@@ -93,9 +109,10 @@ require_once('header.php');
     </section>
 </main>
 <footer>
-    <p>Количество гласных букв на сайте: <? echo $strVowelCount?></p>
-    <p>Количество слов на сайте: <? echo count($arrAll)?></p>
-    <p>День рожденья: <? echo $date?>. Сегодня: <? echo $dateToday ?>. Разница в днях составляет: <? echo $dayDiff?></p>
+    <p>Количество гласных букв на сайте: <? echo vowelCount($strAll)?></p>
+    <p>Количество слов на сайте: <? echo wordCount($strAll)?></p>
+    <p>День рожденья: <? echo $Birthday?>. Сегодня: <? echo $dateToday ?>. Разница в днях составляет: <? echo dayDiff($Birthday)?></p>
+    <p>Числа Фибоначчи через рекурсию: <? fibonachi(0,1); ?></p>
 </footer>
 </body>
 </html>
